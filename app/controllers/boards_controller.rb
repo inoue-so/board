15ba49e245d3baa6c00ad543f921177ab503da47
inoue-so <knight0922basket@gmail.com>
2016-08-28 21:02:28 +0900

    remove boards/destroy.html.erb

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -26,6 +26,9 @@ class BoardsController < ApplicationController
   end
 
   def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'home/show'
 
-  root to: "home#index"
+  root to: "boards#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
